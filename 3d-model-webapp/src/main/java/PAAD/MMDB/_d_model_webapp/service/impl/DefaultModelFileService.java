a5d40a495ea6b5cf78e1fc43388493088bc9d968
boran-architech <boran@architech.hr>
2024-09-01 16:51:53 +0200

    Added mapper, file and its DTO along with controller, service and impl, updated db changelog.xml.

--- 3d-model-webapp/src/main/java/PAAD/MMDB/_d_model_webapp/service/impl/DefaultModelFileService.java
+++ 3d-model-webapp/src/main/java/PAAD/MMDB/_d_model_webapp/service/impl/DefaultModelFileService.java
@@ -2,11 +2,15 @@ package PAAD.MMDB._d_model_webapp.service.impl;
 
 import PAAD.MMDB._d_model_webapp.models.ModelFile;
 import PAAD.MMDB._d_model_webapp.service.ModelFileService;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.io.IOException;
 import java.util.stream.Stream;
 
+@RestController
+@RequestMapping("/api/files")
 public class DefaultModelFileService implements ModelFileService {
     @Override
     public ModelFile store(MultipartFile file) throws IOException {
